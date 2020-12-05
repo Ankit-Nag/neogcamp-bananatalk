055c2d4f1fda0545249eac32a203292c29638ea4
Ankit-Nag <ankitknag@gmail.com>
2020-12-05 23:43:37 +0530

    Js connections
    Wiring the 3 elements in Javascript

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var btnTranslate = document.querySelector("#btnTranslate");
-console.log(btnTranslate);
+var txtInput = document.querySelector("#input-box");
+var outputDiv = document.querySelector("#output-box");
+
+
+
+function clickEventHandler(){
+    console.log("Clicked!");
+    console.log(txtInput.value);
+}
+
+
+btnTranslate.addEventListener("click",clickEventHandler)
